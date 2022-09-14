34a1dcaff3fdae261da719e58a37c001674d3cee
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-09-14 07:22:43 -0300

    :sparkles: feat(server.ts): create app's server handler
    Added "graceful shutdown" and "let it crash" concepts and used environment variables to capture listen port

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import app from "./app";
+const processId = process.pid;
+const appPort = process.env.PORT || 4000;
+
+const server = app.listen(appPort, () => { console.log(`Server's running on PORT:${appPort}`); });
+
+process.on("SIGINT", (signal) => {
+  console.log(`APP (PID:${processId}) received ${signal}`);
+  server.close(() => { process.exit(0); });
+});
+process.on("SIGTERM", (signal) => {
+  console.log(`APP (PID:${processId}) received ${signal}`);
+  server.close(() => { process.exit(0); });
+});
+
+process.on("uncaughtException", (error, origin) => {
+  console.log(`APP (PID:${processId}) received ${origin}\n${error}`);
+  server.close(() => { process.exit(1); });
+  setTimeout(() => { process.exit(1); }, 1000).unref();
+});
+process.on("uncaughtException", (reason) => {
+  console.log(`APP (PID:${processId}) received ${reason}`);
+  server.close(() => { process.exit(1); });
+  setTimeout(() => { process.exit(1); }, 1000).unref();
+});
