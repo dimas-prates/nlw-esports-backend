0f5724b03044fbdade426a33002ded155a7db100
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-09-23 00:02:59 -0300

    :sparkles: feat(routes - index.ts): add game routes to main index routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { HealthCheck } from "./healthCheckRouter";
-
+import gamesRouter from "./gamesRouter";
 const routes = Router();
 
 routes.use("/healthCheck", new HealthCheck().check);
+routes.use(gamesRouter);
 export default routes;
